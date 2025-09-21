84a899a6dd85ced5cc196919ac886dd556e2dfc7
Nuno Nebeker <nebeker@users.noreply.github.com>
2025-09-21 13:50:02 +0100

    Add Home with CTA.

--- outlinked-client/src/app/app.routes.ts
+++ outlinked-client/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home-component/home-component';
 import { ContentGenerationComponent } from './content-generation/content-generation-component/content-generation-component';
 import { UserInfoComponent } from './user-info/user-info-component/user-info-component';
 
 export const routes: Routes = [
-  { path: '', component: ContentGenerationComponent },
+  { path: '', component: HomeComponent },
   { path: 'generate', component: ContentGenerationComponent },
   { path: 'user-info', component: UserInfoComponent },
 ];
