d5f533015f46020ac7e2a9750e1aac0cf077d111
Nuno Nebeker <nebeker@users.noreply.github.com>
2025-09-28 16:39:54 +0100

    Fix typos and simplify reply prompt.

--- OutLinkedServer/ContentGeneration/PromptSegments.cs
+++ OutLinkedServer/ContentGeneration/PromptSegments.cs
@@ -8,23 +8,23 @@ public static class PromptSegments
         "Your answer will be directly copied and pasted. You must not offer commentary, suggestions or options.";
 
     public const string Reply =
-        "Write an insightful professional replyt to a professional social media post in 20 words or less. Start with a summary and add a piece of your own insight.";
+        "Write an insightful professional replyt to a professional social media post in 20 words or less. Explain the main point and add a piece of your own insight.";
 
     public const string Post =
         "Write an insightful professional social media post in 200 words or less in reference to the quoted post. Start with a summary and add your own insight in a way that subtly shows you're better than the original author.";
 
     public const string AuthorInfo =
-        "You are an experienced {title} in the {indsutry} industry with vast knowledge in the field.";
+        "You are an experienced {title} in the {industry} industry with vast knowledge in the field.";
 
     public const string PlugContent =
         "At the end, make sure readers check your {type} called {title} available at {address}.";
 
-    public const string UserPromptSeparator = "Don't deivde your response into sections. The original post starts here:";
+    public const string UserPromptSeparator = "Don't divide your response into sections. The original post starts here:";
 
     public class Placeholders
     {
         public const string AuthorTitle = "{title}";
-        public const string AuthorIndustry = "{indsutry}";
+        public const string AuthorIndustry = "{industry}";
         public const string PlugType = "{type}";
         public const string PlugTitle = "{title}";
         public const string PlugAddress = "{address}";
