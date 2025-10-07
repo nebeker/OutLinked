2ed69328b75691eea4643ef7dc89013c174c45f3
Nuno Nebeker <nebeker@users.noreply.github.com>
2025-10-07 18:05:00 +0100

    Set up ng environments.

--- outlinked-client/src/app/content-generation/content-generation-service.ts
+++ outlinked-client/src/app/content-generation/content-generation-service.ts
@@ -3,14 +3,16 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IContentRequest } from './models/content-request-model';
 import { IContentResponse } from './models/content-response-model';
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class ContentGenerationService {
-  private webApiUrl = 'http://outlinkedserver:8080/ContentGeneration'; //'http://localhost:5098/ContentGeneration';
+  private webApiUrl = environment.serverUrl;
   constructor(private http: HttpClient) {}
 
   public generateContent(request: IContentRequest) {
+    console.log(this.webApiUrl)
     return this.http.post<IContentResponse>(this.webApiUrl, request);
   }
 }
